2c5b4c5ae44568e1228313c7e10d376fac9be08a
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:31:14 +0200

    Add project files.

---
+++ masivi/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+
+int[] numbers = new int[5];
+
+
+numbers[0] = 10;
+numbers[1] = 20;
+numbers[2] = 30;
+//int first = numbers[];
+
+//string.Join(s kakvo da gi razdelq, masiva
+
+//Console.WriteLine(string.Join('-', numbers));
+
+//for (int i = 0; i < numbers.Length; i++)
+{ 
+
+
+   // int current = numbers[i];
+   // Console.WriteLine(current);
+
+}
+
+//foreach (int num in numbers)
+{ 
+
+
+    //Console.WriteLine(num);
+
+
+
+}
+
+//string[] fruits = { "banan" };
+
+
+
+
+
+string[] names =  new string[] {  "Gogo", "Pepo", "Mitko"};
+
+
+foreach (string name in names)
+{
+
+    Console.Write(name + "    ");
+
+
+}
